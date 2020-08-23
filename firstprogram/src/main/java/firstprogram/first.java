5594f1a5d03e7bc00a9eb1c69d6e0b86ff03a7fe
sunilna <sunilna@M4L-FAC-SUNILN.mul.manipal.net>
2020-08-23 11:41:38 +0530

    second commit

--- firstprogram/src/main/java/firstprogram/first.java
+++ firstprogram/src/main/java/firstprogram/first.java
@@ -4,6 +4,7 @@ public class first {
 
 	public static void main(String[] args) {
 	System.out.println("my first maven project");
+	System.out.println("This is second line");
 
 	}
 
--- firstprogram/src/test/java/firstprogramtest/testfirst.java
+++ firstprogram/src/test/java/firstprogramtest/testfirst.java
@@ -4,6 +4,7 @@ public class testfirst {
 
 	public static void main(String[] args) {
 		System.out.println("this is testing");
+		System.out.println("this is second");
 
 	}
 
