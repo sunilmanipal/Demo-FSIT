f4833f0a94984f4b003dd6576c82919eebcdc8ab
sunilna <sunilna@M4L-FAC-SUNILN.mul.manipal.net>
2020-08-23 11:35:27 +0530

    First Commit

---
+++ firstprogram/src/main/java/firstprogram/first.java
@@ -0,0 +1,10 @@
+package firstprogram;
+
+public class first {
+
+	public static void main(String[] args) {
+	System.out.println("my first maven project");
+
+	}
+
+}
---
+++ firstprogram/src/test/java/firstprogramtest/testfirst.java
@@ -0,0 +1,10 @@
+package firstprogramtest;
+
+public class testfirst {
+
+	public static void main(String[] args) {
+		System.out.println("this is testing");
+
+	}
+
+}
